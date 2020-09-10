13123bb4a4817d0e6c9e8ff6c0ff8e2968c5dda2
rrsalerno21 <rrsalerno21@gmail.com>
2020-09-09 20:49:53 -0700

    added qr params to welcome page

--- client/src/pages/Welcome.js
+++ client/src/pages/Welcome.js
@@ -1,14 +1,19 @@
 import React, { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
 // import API from "./../utils/API";
 import Container from "../components/Container";
 // import { Link } from "react-router-dom";
 // import { useAuth } from "../utils/auth";
 
 function Welcome() {
-
+  const { company_id, table_num } = useParams();
   return (
     <Container>
       <h1> Welcome PAGE </h1>
+      <p>
+        Company Id: {company_id} <br />
+        Table Num: {table_num}
+      </p>
     </Container>
   );
 }
