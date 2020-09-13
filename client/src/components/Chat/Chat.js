97a8ee318c509245c9d0ee488fa4b8054b93eb23
Nick Kim <nicholaskim1989@gmail.com>
2020-09-12 21:25:26 -0700

    fixed chat comp. displaying messages for the wrong table

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,74 +1,80 @@
 // 'useEffect' allows you to perform side effects in function components
 import React, { useState, useEffect } from "react";
 // This module will be used for retrieving data from the URL
-import queryString from 'query-string';
 import io from "socket.io-client";
-
-import TextContainer from '../TextContainer/TextContainer';
 import Messages from '../Messages/Messages';
 import InfoBar from '../InfoBar/InfoBar';
 import Input from '../Input/Input';
-
 import './Chat.css';
 
 let socket;
 
 const Chat = ({ name, room }) => {
-    // const [name, setName] = useState('');
-    // const [room, setRoom] = useState('');
-    const [users, setUsers] = useState('');
-    const [message, setMessage] = useState('');
-    const [messages, setMessages] = useState([]);
-    const ENDPOINT = "http://localhost:3000/";
+  // const [name, setName] = useState('');
+  // const [room, setRoom] = useState('');
+  const [users, setUsers] = useState('');
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
+  const ENDPOINT = "http://localhost:3001/";
 
-    useEffect(() => {
-        // const { name, room } = queryString.parse(location.search);
+  useEffect(() => {
+    // const { name, room } = queryString.parse(location.search);
 
-        socket = io(ENDPOINT);
+    socket = io(ENDPOINT);
 
-        // setRoom(room);
-        // setName(name)
+    // setRoom(room);
+    // setName(name)
 
-        socket.emit('join', { name, room }, (error) => {
-            if (error) {
-                alert(error);
-            }
-        });
+    socket.emit('join', { name, room: String(room) }, (error) => {
+      if (error) {
+        alert(error);
+      }
+    });
 
-        return function cleanup() {
-          socket.disconnect('disconnect');
-        }
-    }, [ENDPOINT, name, room]);
+    socket.on('message', message => {
+      setMessages(messages => [...messages, message]);
+    });
 
-    useEffect(() => {
-        socket.on('message', message => {
-            setMessages(messages => [...messages, message]);
-        });
+    socket.on("roomData", ({ users }) => {
+      setUsers(users);
+    });
 
-        socket.on("roomData", ({ users }) => {
-            setUsers(users);
-        });
-    }, []);
-
-    const sendMessage = (event) => {
-        event.preventDefault();
-
-        if (message) {
-            socket.emit('sendMessage', message, () => setMessage(''));
-        }
+    return function cleanup() {
+      socket.disconnect('disconnect');
+      setMessages([]);
     }
+  }, [ENDPOINT, name, room]);
 
-    return (
-        // <div className="outerContainer">
-            <div className="container">
-              
-                <InfoBar room={room} />
-                <Messages messages={messages} name={name} />
-                <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
-            </div>
-        //     {/* <TextContainer users={users} />
-        // </div> */}
-    );
+  // useEffect(() => {
+  //     socket.on('message', message => {
+  //       console.log(message);
+  //         setMessages(messages => [...messages, message]);
+  //     });
+
+  //     socket.on("roomData", ({ users }) => {
+  //         setUsers(users);
+  //     });
+  // }, []);
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+
+    if (message) {
+      socket.emit('sendMessage', message, () => setMessage(''));
+    }
+  }
+
+  return (
+    // <div className="outerContainer">
+    <div className="container">
+
+      <InfoBar room={room} />
+      <Messages messages={messages} name={name} />
+      <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
+    </div>
+    //     {/* <TextContainer users={users} />
+    // </div> */}
+  );
 }
 
 export default Chat;
