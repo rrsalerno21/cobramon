0bfd5f41f5b7ead7005233329857bea0a6f3cc4d
Kelly Gowing <Gowing.kelly@yahoo.com>
2020-09-14 19:38:48 -0700

    thank you page styling

--- client/src/pages/ThankYou/index.js
+++ client/src/pages/ThankYou/index.js
@@ -1,15 +1,15 @@
 import React, { useState, useEffect } from "react";
 // import API from "./../utils/API";
-import Container from "../components/Container";
 // import { Link } from "react-router-dom";
 // import { useAuth } from "../utils/auth";
+import "./thankyou.css";
 
 function ThankYou() {
 
   return (
-    <Container>
-      <h1> ThankYou PAGE </h1>
-    </Container>
+    <div className="thankyou-container">
+      <h1 className="thankyou">Thank you for visiting company name</h1>
+    </div>
   );
 }
 
